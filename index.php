4604736483c67cfa023f933e983334641ad8707c
manishrsilwal <manishraj.silwal@gmail.com>
2018-07-12 00:18:45 +0545

    Latest Changes

--- index.php
+++ index.php
@@ -2,19 +2,31 @@
 <html>
 <head>
 	<title>Smart School</title>
-	<link href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 </head>
 <body>
 	<div class="header">
 		<div class="col-sm-4">
-			<h3>Smart School</h3>
+			<font size="5">Smart School</font>
 		</div>
 		<div class="col-sm-4">
+			<div class="form-group ">
+			    <form action="#">
+			    	<div class="input-group">
+			      		<input type="text" class="form-control" placeholder="Search.." name="search">
+		      			<div class="input-group-btn">
+			      			<button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+			      		</div>
+			      	</div>
+			    </form>
+		  	</div>
 		</div>
 		<div class="col-sm-4">
+			<button type="" class="btn btn-default"><i class="fas fa-bell"></i></button> | <button type="" class="btn btn-default"><i class="fas fa-cog"></i></button> | <span><img src="images/book.jpg" class="img-circle" height="35" width="35">&nbsp;Manish Raj Silwal&nbsp;<button type="" class="btn btn-default"><i class="fas fa-chevron-circle-down"></i></button></span>
 		</div>
 	</div>
 </body>
