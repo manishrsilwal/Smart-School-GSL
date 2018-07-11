e9b7c3cbae0859d17f9e0a1a4e90ee972f33b3e3
manishrsilwal <manishraj.silwal@gmail.com>
2018-07-11 20:13:02 +0545

    Initialization

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart School</title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+</head>
+<body>
+	<div class="header">
+		<div class="col-sm-4">
+			<h3>Smart School</h3>
+		</div>
+		<div class="col-sm-4">
+		</div>
+		<div class="col-sm-4">
+		</div>
+	</div>
+</body>
+</html>
