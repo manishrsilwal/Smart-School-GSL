9cc5acb10213999e80ba4ed8311bd65c4a5c90f0
manishrsilwal <manishraj.silwal@gmail.com>
2018-07-24 16:27:09 +0545

    Updated Homework Section

--- teacher/footer.php
+++ teacher/footer.php
@@ -1,3 +1,4 @@
 
+<script type="text/javascript" src="../js/dropdown.js"></script>
 </body>
 </html>
---
+++ teacher/homeworkcheck.php
@@ -0,0 +1,34 @@
+<?php
+	include("header.php");
+?>
+
+<div class="row content">
+	<div class="col-sm-2 sidenav">
+		<center>
+			<div class="figure"><img src="../images/logo.jpg" class="img-circle" height="150" width="150"></div>
+			<div class="fig-caption">Kathmandu High School<br /><div class="sidenav-tagline">Quality Education for Leadership</div></div>
+		</center>
+		<a href="../teacher"><i class="fas fa-home"></i>&nbsp;&nbsp;&nbsp;Home</a>
+		<a href="../teacher/marksentry.php"><i class="fas fa-marker"></i>&nbsp;&nbsp;&nbsp;Marks Entry</a>
+		<a href="../teacher/attendance.php"><i class="fas fa-book-open"></i>&nbsp;&nbsp;&nbsp;Attendance</a>
+		<a class="btn dropdown-btn"><i class="fas fa-edit dropdown-toggle"></i>&nbsp;&nbsp;&nbsp;Assignments &nbsp;&nbsp;&nbsp;<span class="caret"></span></a>
+	  	<div class="dropdown-container">
+		    <a href="../teacher/homeworkentry.php" class="active"><i class="fas fa-pencil-alt"></i>&nbsp;&nbsp;&nbsp;Assign the Work</a>
+		    <a href="../teacher/homeworkcheck.php"><i class="fas fa-check"></i>&nbsp;&nbsp;&nbsp;Check the Work</a>
+		</div>
+		<a href="../teacher/student-info.php"><i class="fas fa-user"></i>&nbsp;&nbsp;&nbsp;Student Info</a>
+		<a href="#"><i class="fas fa-envelope"></i>&nbsp;&nbsp;&nbsp;Messages</a>
+	</div>
+
+	<div class="col-sm-10 description">
+		<div class="page-header title">Check the Homework<p class="home-tagline">Check the assignments</p></div>
+		
+		<h1>Under Construction</h1>		
+
+	</div>
+</div>
+
+
+<?php
+	include("footer.php");
+?>
